d7ffc20bae459566f338a3689b28f529800fd141
Benjamin Warner <me@benjaminwarner.dev>
2025-06-16 00:17:33 -0500

    add lion triton kernel

--- optimi/lion.py
+++ optimi/lion.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Benjamin Warner
+# Copyright (c) 2023-present Benjamin Warner
 # SPDX-License-Identifier: MIT
 
 # Based on PyTorch Optimizers
@@ -10,16 +10,28 @@
 # Learning rate decoupled weight decay inspired by Composer's `DecoupledLionW` & `DecoupledAdamW`
 # Composer - Apache License 2.0 - Copyright (c) 2022 MosaicML Composer authors
 
-from __future__ import annotations
+# Triton kernels inspired by:
+# AdamW-Triton-PyTorch - MIT License - Copyright (c) 2024 Less Wright - https://github.com/lessw2020/AdamW-Triton-PyTorch
+# lion-pytorch - MIT License - Copyright (c) 2023 Phil Wang - https://github.com/lucidrains/lion-pytorch
 
-from typing import Any, Callable, Iterable
+from collections.abc import Callable, Iterable
+from typing import Any
 
 import torch
 from torch import Tensor
 from torch.optim.optimizer import _default_to_fused_or_foreach
 from torch.utils._foreach_utils import _group_tensors_by_device_and_dtype
 
+try:
+    import triton
+    import triton.language as tl
+
+    SUPPORTS_TRITON = True
+except ImportError:
+    SUPPORTS_TRITON = False
+
 from optimi.optimizer import OptimiOptimizer
+from optimi.utils import device_guard
 
 __all__ = ["Lion", "lion"]
 
@@ -42,7 +54,9 @@ class Lion(OptimiOptimizer):
             precision (float16 or bfloat16). If unspecified, automatically applies for low precision
             parameters (default: None)
         foreach: Enables the foreach implementation. If unspecified, tries to use foreach over
-            for-loop implementation since it is significantly faster (default: None)
+            for-loop implementation since it can be significantly faster (default: None)
+        triton: Enables Triton implementation. If unspecified, tries to use Triton as it is
+            significantly faster than both for-loop and foreach implementations (default: None)
         gradient_release: Fuses optimizer step and zero_grad as part of the parameter's backward
             pass. Requires model hooks created with `register_gradient_release`. Incompatible with
             closure (default: False)
@@ -58,6 +72,7 @@ class Lion(OptimiOptimizer):
         max_lr: float | None = None,
         kahan_sum: bool | None = None,
         foreach: bool | None = None,
+        triton: bool | None = None,
         gradient_release: bool = False,
     ):
         if not 0.0 <= betas[0] < 1.0:
@@ -74,6 +89,7 @@ class Lion(OptimiOptimizer):
             max_lr=max_lr,
             kahan_sum=kahan_sum,
             foreach=foreach,
+            triton=triton,
             gradient_release=gradient_release,
             setup=False,
         )
@@ -86,6 +102,8 @@ class Lion(OptimiOptimizer):
             if (group["kahan_sum"] or group["kahan_sum"] is None) and param.dtype in [torch.float16, torch.bfloat16]:
                 state["kahan_comp"] = torch.zeros_like(param, memory_format=torch.preserve_format)
                 group["kahan_sum"] = True
+            elif group["triton"]:
+                state["kahan_comp"] = torch.zeros(1, dtype=param.dtype, device=param.device)
             else:
                 state["kahan_comp"] = None
 
@@ -145,6 +163,7 @@ class Lion(OptimiOptimizer):
                     max_lr=group["max_lr"],
                     kahan_sum=group["kahan_sum"],
                     foreach=group["foreach"],
+                    triton=group["triton"],
                     gradient_release=False,
                     optimizer_accumulation=False,
                 )
@@ -166,6 +185,7 @@ class Lion(OptimiOptimizer):
                 max_lr=group["max_lr"],
                 kahan_sum=group["kahan_sum"],
                 foreach=False,
+                triton=group["triton"],
                 gradient_release=True,
                 optimizer_accumulation=self._optimizer_accumulation,
             )
@@ -187,6 +207,7 @@ def lion(
     max_lr: float | None = None,
     kahan_sum: bool = False,
     foreach: bool = False,
+    triton: bool = False,
     gradient_release: bool = False,
     optimizer_accumulation: bool = False,
 ):
@@ -207,6 +228,7 @@ def lion(
         max_lr: Maximum scheduled learning rate for `decouple_lr`
         kahan_sum: Enables Kahan summation for low precision `params`
         foreach: Enables the faster foreach implementation
+        triton: Enables the faster Triton implementation
         gradient_release: Fuses optimizer step as part of the parameter's backward pass
         optimizer_accumulation: Accumulate gradients into state during gradient release step
     """
@@ -224,12 +246,20 @@ def lion(
     if kahan_comps is None:
         kahan_comps = [None] * len(params)
 
-    if foreach:
-        func = _foreach_lion
-    elif gradient_release:
-        func = _single_param_lion
+    if gradient_release:
+        if triton:
+            func = _single_param_triton_lion
+        elif foreach:
+            raise ValueError(f"Gradient release {gradient_release=} and foreach {foreach=} cannot be used together")
+        else:
+            func = _single_param_lion
     else:
-        func = _single_lion
+        if triton:
+            func = _triton_lion
+        elif foreach:
+            func = _foreach_lion
+        else:
+            func = _single_lion
 
     func(
         params,
@@ -357,3 +387,156 @@ def _foreach_lion(
         else:
             # Lion step
             torch._foreach_add_(dev_params, dev_updates, alpha=-lr)
+
+
+if SUPPORTS_TRITON:
+
+    @triton.autotune(
+        configs=[
+            triton.Config({"BLOCK_SIZE": 128}, num_warps=4),
+            triton.Config({"BLOCK_SIZE": 256}, num_warps=4),
+            triton.Config({"BLOCK_SIZE": 512}, num_warps=8),
+            triton.Config({"BLOCK_SIZE": 1024}, num_warps=8),
+        ],
+        key=["n_elements", "kahan_sum", "decouple_wd", "do_weight_decay", "update_parameters"],
+        restore_value=["param_ptr", "exp_avg_ptr", "kahan_ptr"],
+    )
+    @triton.jit
+    def _lion_kernel(
+        param_ptr,
+        grad_ptr,
+        exp_avg_ptr,
+        kahan_ptr,
+        lr,
+        beta1_comp,
+        beta2_comp,
+        weight_decay,
+        do_weight_decay: tl.constexpr,
+        kahan_sum: tl.constexpr,
+        update_parameters: tl.constexpr,
+        n_elements,
+        BLOCK_SIZE: tl.constexpr,
+    ):
+        pid = tl.program_id(0)
+        block_start = pid * BLOCK_SIZE
+        offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+
+        # Load data
+        param = tl.load(param_ptr + offsets, mask=mask)
+        # For low precision, with or without Kahan summation, we want all
+        # computation except for the parameter updates to occur in float32.
+        grad = tl.load(grad_ptr + offsets, mask=mask).to(tl.float32)
+        exp_avg = tl.load(exp_avg_ptr + offsets, mask=mask).to(tl.float32)
+
+        # decoupled weight decay or fully decoupled weight decay
+        if do_weight_decay and update_parameters:
+            param = tl.cast(param * weight_decay, param.dtype)
+
+        # parameter update value
+        tmp = exp_avg + beta1_comp * (grad - exp_avg)
+        update = tl.where(tmp > 0, 1.0, tl.where(tmp < 0, -1.0, 0.0))
+
+        # update gradient moving average
+        exp_avg = exp_avg + beta2_comp * (grad - exp_avg)
+
+        if update_parameters:
+            if kahan_sum:
+                # load kahan compensation, casting to fp32
+                kahan_comp = tl.load(kahan_ptr + offsets, mask=mask).to(tl.float32)
+
+                # Lion step
+                kahan_comp = kahan_comp - (lr * update)
+
+                # update weights with downcasted kahan update
+                prev_param = param
+                param = param + tl.cast(kahan_comp, param.dtype)
+
+                # save error back to kahan compensation for next iteration
+                kahan_comp = kahan_comp + prev_param.to(tl.float32) - param.to(tl.float32)
+
+                # store kahan compensation
+                tl.store(kahan_ptr + offsets, tl.cast(kahan_comp, param.dtype), mask=mask)
+            else:
+                # Standard Lion step
+                param = param + tl.cast((-lr * update), param.dtype)
+
+            # Store updated parameters
+            tl.store(param_ptr + offsets, param, mask=mask)
+
+        # Optionally downcast exp_avg to param.dtype
+        tl.store(exp_avg_ptr + offsets, tl.cast(exp_avg, param.dtype), mask=mask)
+
+    def _triton_lion(
+        params: list[Tensor],
+        grads: list[Tensor],
+        exp_avgs: list[Tensor],
+        kahan_comps: list[Tensor],
+        *,
+        lr: float,
+        beta1_comp: float,
+        beta2_comp: float,
+        weight_decay: float,
+        kahan_sum: bool = False,
+        **kwargs,
+    ):
+        for i, param in enumerate(params):
+            grad = grads[i]
+            exp_avg = exp_avgs[i]
+            kahan_comp = kahan_comps[i]
+
+            n_elements = param.numel()
+            grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)  # noqa: E731
+
+            # Without this Triton tries to launch from cuda:0 and we get
+            # ValueError: Pointer argument (at 0) cannot be accessed from Triton (cpu tensor?)
+            with device_guard(param):
+                _lion_kernel[grid](
+                    param_ptr=param,
+                    grad_ptr=grad,
+                    exp_avg_ptr=exp_avg,
+                    kahan_ptr=kahan_comp,
+                    lr=lr,
+                    beta1_comp=beta1_comp,
+                    beta2_comp=beta2_comp,
+                    weight_decay=weight_decay,
+                    do_weight_decay=weight_decay != 0.0,
+                    kahan_sum=kahan_sum and param.dtype in [torch.float16, torch.bfloat16],
+                    update_parameters=True,
+                    n_elements=n_elements,
+                )
+
+    def _single_param_triton_lion(
+        param: Tensor,
+        grad: Tensor,
+        exp_avg: Tensor,
+        kahan_comp: Tensor | None,
+        *,
+        lr: float,
+        beta1_comp: float,
+        beta2_comp: float,
+        weight_decay: float,
+        kahan_sum: bool,
+        update_parameters: bool,
+        **kwargs,
+    ):
+        n_elements = param.numel()
+        grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)  # noqa: E731
+
+        # Without this Triton tries to launch from cuda:0 and we get
+        # ValueError: Pointer argument (at 0) cannot be accessed from Triton (cpu tensor?)
+        with device_guard(param):
+            _lion_kernel[grid](
+                param_ptr=param,
+                grad_ptr=grad,
+                exp_avg_ptr=exp_avg,
+                kahan_ptr=kahan_comp,
+                lr=lr,
+                beta1_comp=beta1_comp,
+                beta2_comp=beta2_comp,
+                weight_decay=weight_decay,
+                do_weight_decay=weight_decay != 0.0,
+                kahan_sum=kahan_sum and param.dtype in [torch.float16, torch.bfloat16],
+                update_parameters=update_parameters,
+                n_elements=n_elements,
+            )
